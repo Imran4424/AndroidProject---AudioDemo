17b4b4fa6d92e84c26ac5f0a0407b4e7b46dec48
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-08-16 18:36:48 +0600

    seekbar added

--- app/src/main/java/com/luminous/audiodemo/MainActivity.java
+++ app/src/main/java/com/luminous/audiodemo/MainActivity.java
@@ -2,22 +2,52 @@ package com.luminous.audiodemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    private final String PLAY_TEXT = "PLAY";
+    private final String PAUSE_TEXT = "PAUSE";
+
+    MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+    Button playPauseButton;
+    SeekBar playTimeSeekBar;
+    SeekBar volumeSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.sheyal_sharosh);
+        playPauseButton = (Button) findViewById(R.id.buttonPlayPause);
+        playTimeSeekBar = (SeekBar) findViewById(R.id.seekBarPlayStatus);
+        volumeSeekBar = (SeekBar) findViewById(R.id.seekVolume);
+
+        playPauseButton.setText(PLAY_TEXT);
     }
 
-    public void play(View v) {
+    public void playPause(View v) {
+        if(playPauseButton.getText().toString().equals(PLAY_TEXT)) {
+            mediaPlayer.start();
+            playPauseButton.setText(PAUSE_TEXT);
+        } else {
+            mediaPlayer.pause();
+            playPauseButton.setText(PLAY_TEXT);
+        }
+
 
     }
     
-    public void pause(View v) {
+    public void stop(View v) {
+        mediaPlayer.stop();
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.sheyal_sharosh);
+        playPauseButton.setText(PLAY_TEXT);
     }
 }
